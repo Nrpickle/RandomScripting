bc375e88046956892c6ff0a1b6eb7dfa0180313e
Nick McComb <nrpickle@gmail.com>
2016-04-19 11:15:02 -0700

    Added .zip capability

--- _processAltiumOutputs.py
+++ _processAltiumOutputs.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import zipfile
 
 print "Process Altium Outputs Script v1"
 print "Written by Nick McComb | www.nickmccomb.net"
@@ -10,6 +11,18 @@ if not os.path.exists(dirName):
     os.makedirs(dirName)
 
 PCBFileName = ""
+
+def make_zipfile(output_filename, source_dir):
+    relroot = os.path.abspath(os.path.join(source_dir, os.pardir))
+    with zipfile.ZipFile(output_filename, "w", zipfile.ZIP_DEFLATED) as zip:
+        for root, dirs, files in os.walk(source_dir):
+            # add directory (needed for empty dirs)
+            zip.write(root, os.path.relpath(root, relroot))
+            for file in files:
+                filename = os.path.join(root, file)
+                if os.path.isfile(filename): # regular files only
+                    arcname = os.path.join(os.path.relpath(root, relroot), file)
+                    zip.write(filename, arcname)
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
@@ -44,3 +57,6 @@ for f in files:
                 print "Renamed " + f + " to " + f.split(".", 1)[0] + ".XLN"
                 shutil.move(f.split(".", 1)[0] + ".XLN", dirName + "/" + f.split(".", 1)[0] + ".XLN")
                 print "moved .XLN!"
+
+make_zipfile(dirName + ".zip", dirName)
+print "Compressed folder to " + dirName + ".zip")
